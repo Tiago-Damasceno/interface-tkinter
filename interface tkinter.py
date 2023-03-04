b472a92c15e852b21df5e8acbf058013cf59473d
Tiago Damasceno <devtiago01@gmail.com>
2023-03-03 23:11:48 -0300

    criado e teste janela

--- interface tkinter.py
+++ interface tkinter.py
@@ -1,8 +1,19 @@
 from tkinter import *
+import tkinter
 
 janela = Tk()
+janela.title('janela de update')
+janela.geometry("400x1000")
 
+texto_orientação = Label(janela, text='digite seu nome')
+texto_orientação.grid(column=0, row=0, padx=10, pady=10)
+botão = Button(janela, text='digite seu nome')
+botão.grid(column=100, row=200, padx=0, pady=150)
+texto_orientação3 = Label(janela, text='digite seu nome')
+texto_orientação3.grid(column=10, row=10, padx=10, pady=20)
 
+mensagem = tkinter.messagebox.showinfo(title='ola', message='oi')
+mensagem.grid(column=100, row=100)
 
 
 

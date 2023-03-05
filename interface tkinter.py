1cc341b23a4e33eaba839e1d6a21614fdc531bea
Tiago Damasceno <devtiago01@gmail.com>
2023-03-04 21:25:49 -0300

    mudei 'import as tk'

--- interface tkinter.py
+++ interface tkinter.py
@@ -1,19 +1,18 @@
-from tkinter import *
-import tkinter
+import tkinter as tk
 
-janela = Tk()
+janela = tk.Tk()
 janela.title('janela de update')
 janela.geometry("400x1000")
 
-texto_orientação = Label(janela, text='digite seu nome')
+texto_orientação = tk.Label(janela, text='digite seu nome')
 texto_orientação.grid(column=0, row=0, padx=10, pady=10)
-botão = Button(janela, text='digite seu nome')
+botão = tk.Button(janela, text='digite seu nome')
 botão.grid(column=100, row=200, padx=0, pady=150)
-texto_orientação3 = Label(janela, text='digite seu nome')
+texto_orientação3 = tk.Label(janela, text='digite seu nome')
 texto_orientação3.grid(column=10, row=10, padx=10, pady=20)
 
-mensagem = tkinter.messagebox.showinfo(title='ola', message='oi')
-mensagem.grid(column=100, row=100)
+#mensagem = tk.tkinter.messagebox.showinfo(title='ola', message='oi')
+#mensagem.grid(column=100, row=100)
 
 
 

66f46d363b8d45dfd5aa57a3074a5b96eca25363
Tiago Damasceno <devtiago01@gmail.com>
2023-03-05 23:24:49 -0300

    mrp

--- interface tkinter.py
+++ interface tkinter.py
@@ -1,18 +1,18 @@
 import tkinter as tk
 
 janela = tk.Tk()
-janela.title('janela de update')
-janela.geometry("400x1000")
+janela.title('caixa de login')
 
-texto_orientação = tk.Label(janela, text='digite seu nome')
-texto_orientação.grid(column=0, row=0, padx=10, pady=10)
-botão = tk.Button(janela, text='digite seu nome')
-botão.grid(column=100, row=200, padx=0, pady=150)
-texto_orientação3 = tk.Label(janela, text='digite seu nome')
-texto_orientação3.grid(column=10, row=10, padx=10, pady=20)
+texto_orientação = tk.Label(janela, text='digite seu nome:',fg='white', bg='black', width=15, height=5)
+texto_orientação.grid(column=0, row=0)
+texto_orientação2 = tk.Label(janela, text='email',  width=15, height=5)
+texto_orientação.grid(column=1, row=3)
 
-#mensagem = tk.tkinter.messagebox.showinfo(title='ola', message='oi')
-#mensagem.grid(column=100, row=100)
+resposta = tk.Entry()
+resposta.grid(column=0, row=1)
+resposta2 = tk.Entry()
+resposta2.grid(column=0, row=4)
+
 
 
 
